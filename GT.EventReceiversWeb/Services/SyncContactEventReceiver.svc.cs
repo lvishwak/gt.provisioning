19e568187281ed241e9da81ea9f9b42573660c13
Lokesh Vishwakarma <lokesh_vishwakarma@hotmail.com>
2017-07-13 12:29:58 +0530

    Update remote event receiver code.

--- GT.EventReceiversWeb/Services/SyncContactEventReceiver.svc.cs
+++ GT.EventReceiversWeb/Services/SyncContactEventReceiver.svc.cs
@@ -18,13 +18,17 @@ namespace GT.EventReceiversWeb.Services
         {
             SPRemoteEventResult result = new SPRemoteEventResult();
 
-            using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            switch (properties.EventType)
             {
-                if (clientContext != null)
-                {
-                    clientContext.Load(clientContext.Web);
-                    clientContext.ExecuteQuery();
-                }
+                case SPRemoteEventType.AppInstalled:
+                    HandleAppInstalled(properties);
+                    break;
+                case SPRemoteEventType.AppUninstalling:
+                    HandleAppUninstalling(properties);
+                    break;
+                case SPRemoteEventType.ItemAdded:
+                    HandleItemAdded(properties);
+                    break;
             }
 
             return result;
@@ -36,12 +40,64 @@ namespace GT.EventReceiversWeb.Services
         /// <param name="properties">Holds information about the remote event.</param>
         public void ProcessOneWayEvent(SPRemoteEventProperties properties)
         {
-            using (ClientContext clientContext = TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            switch (properties.EventType)
+            {
+                case SPRemoteEventType.ItemAdded:
+                    HandleItemAdded(properties);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles when an app is installed.  Activates a feature in the
+        /// host web.  The feature is not required.  
+        /// Next, if the Jobs list is
+        /// not present, creates it.  Finally it attaches a remote event
+        /// receiver to the list.  
+        /// </summary>
+        /// <param name="properties"></param>
+        private void HandleAppInstalled(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext =
+                TokenHelper.CreateAppEventClientContext(properties, false))
             {
                 if (clientContext != null)
                 {
-                    clientContext.Load(clientContext.Web);
-                    clientContext.ExecuteQuery();
+                    new RemoteEventReceiverManager().AssociateRemoteEventsToHostWeb(clientContext);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the remote event receiver from the list and 
+        /// adds a new item to the list.
+        /// </summary>
+        /// <param name="properties"></param>
+        private void HandleAppUninstalling(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext =
+                TokenHelper.CreateAppEventClientContext(properties, false))
+            {
+                if (clientContext != null)
+                {
+                    new RemoteEventReceiverManager().RemoveEventReceiversFromHostWeb(clientContext);
+                }
+            }
+        }
+
+        // <summary>
+        /// Handles the ItemAdded event by modifying the Description
+        /// field of the item.
+        /// </summary>
+        /// <param name="properties"></param>
+        private void HandleItemAdded(SPRemoteEventProperties properties)
+        {
+            using (ClientContext clientContext =
+                TokenHelper.CreateRemoteEventReceiverClientContext(properties))
+            {
+                if (clientContext != null)
+                {
+                    new RemoteEventReceiverManager().ItemAddedToListEventHandler(clientContext, properties.ItemEventProperties.ListId, properties.ItemEventProperties.ListItemId);
                 }
             }
         }
